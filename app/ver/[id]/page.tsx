86daf3391677c30623a934a840eef625352f1309
adonisventuramartinez-ai <adonisventuramartinez@gmail.com>
2026-09-01 21:25:34 -0400

    Add dynamic export to page component

--- app/ver/[id]/page.tsx
+++ app/ver/[id]/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+export const dynamic = "force-dynamic";
 import { useEffect, useRef, useState } from "react";
 import { useParams } from "next/navigation";
 import Link from "next/link";
